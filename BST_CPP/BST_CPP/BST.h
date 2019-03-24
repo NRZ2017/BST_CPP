9a9ba2621b694cd62fe810230072fc51a3c7a11f
Nick Zhao <Nick.Zhao@GMR.local>
2019-03-24 16:32:50 -0700

    Need to fix all small things

--- BST_CPP/BST_CPP/BST.h
+++ BST_CPP/BST_CPP/BST.h
@@ -8,41 +8,47 @@ class BST
 private:
 	int count = 0;
 
-	bool Delete(T value)
+	bool Delete(BST_Node<T>* node)
 	{
-		if (node.ChildCount == 2)
+		if (node->ChildCount == 2)
 		{
-			BST_Node<T> fin = Minimum(*node.RightChild);
-			node.Data = fin.Data;
+			BST_Node<T>* fin = Minimum(node->RightChild.get());
+			node->Data = fin->Data;
 			node = fin;
 		}
-		if (node.ChildCount == 0)
+
+		if (node->ChildCount == 1)
 		{
-			if (node == Root)
+			//connect parent to the 1 child
+
+		}
+		else if (node->ChildCount == 0)
+		{
+			if (node == Root.get())
 			{
 				Root == nullptr;
 			}
-			else if (node.IsLeftChild)
+			else if (node->IsLeftChild)
 			{
-				node.Parent->LeftChild == nullptr;
+				node->Parent->LeftChild = nullptr;
 			}
-			else if (node.IsRightChild)
+			else if (node->IsRightChild)
 			{
-				node.Parent->RightChild == nullptr;
+				node->Parent->RightChild = nullptr;
 			}
 
 		}
 	}
 
-	BST_Node<T> Minimum(BST_Node<T> node)
+	BST_Node<T>* Minimum(BST_Node<T>* node)
 	{
-		while (node.LeftChild != nullptr)
+		while (node->LeftChild != nullptr)
 		{
-			node = *node.LeftChild;
+			node = node->LeftChild.get();
 		}
 		return node;
 	}
-	
+
 	BST_Node<T> Maximum(BST_Node<T> node)
 	{
 		while (node.RightChild != nullptr)
@@ -52,14 +58,22 @@ private:
 		return node;
 	}
 
-	BST_Node<T> Find(T value)
+	BST_Node<T>* Find(T value)
 	{
-		BST_Node<T> curr = Root;
-		while (curr != null)
+		BST_Node<T>* curr = Root.get();
+		while (curr != nullptr)
 		{
-			if (curr.Data < value)
+			if (curr->Data < value)
 			{
-				curr = curr.LeftChild;
+				curr = curr->LeftChild.get();
+			}
+			else if (curr->Data > value)
+			{
+				curr = curr->RightChild.get();
+			}
+			else if (curr->Data == 0)
+			{
+				return curr;
 			}
 		}
 	}
@@ -95,15 +109,52 @@ public:
 			return false;
 		}
 	}
-	
+
 	bool Delete(T value)
 	{
-		BST_Node<T> toDelete = 
+		BST_Node<T>* toDelete = Find(value);
+		if (toDelete == nullptr)
+		{
+			return false;
+		}
+		Delete(toDelete);
+		count--;
+		return true;
 	}
 
-	
-	
+	void Insert(T value)
+	{
+		count++;
+		if (Root == nullptr)
+		{
+			Root = new BST_Node<T>(value);
+			return;
+		}
+		BST_Node<T>* curr = Root.get();
+		while (curr != nullptr)
+		{
+			if (value < curr->Data)
+			{
+				curr->LeftChild = new BST_Node<T>(value);
+				break;
+			}
+			curr = curr->LeftChild.get();
 
-	
+			else
+			{
+				if (curr.RightChild != nullptr)
+				{
+					curr.RightChild = new BST_Node<T>(value);
+					break;
+				}
+				curr = *curr.RightChild;
+			}
+		}
+
+
+
+
+
+	}
 
 };
